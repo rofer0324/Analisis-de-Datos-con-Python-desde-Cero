a32ca1efd8bd2ec29450b744569e2b01a680a13f
David <davidrodfer0324@gmail.com>
2024-01-17 19:05:26 -0500

    Añadir Archivos: G_barra

---
+++ Seccion 11 - Matplotlib/G_Barra.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+alturas = [10, 20, 30, 40, 50]
+x =  range(len(alturas))
+
+plt.bar(x, alturas, width=0.5, color='green')
+plt.title('Gráfico de barras')
+plt.xlabel('Eje X')
+plt.ylabel('Eje Y')
+plt.show()
