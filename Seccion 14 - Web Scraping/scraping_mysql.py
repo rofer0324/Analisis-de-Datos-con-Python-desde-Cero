9cce857f28ba36708036a7cb5faad8f855c6eefd
David <davidrodfer0324@gmail.com>
2024-03-01 00:01:19 -0500

    Añadir: scraping_mysql.py [Completo], scraping.sql [Archivo de consulta de la DBA]

--- Seccion 14 - Web Scraping/scraping_mysql.py
+++ Seccion 14 - Web Scraping/scraping_mysql.py
@@ -6,7 +6,20 @@ conexion = mysql.connector.connect(
     user = "root",
     password = "password",
     host = "localhost",
-    database = "scraping"   
+    database = "scraping"  
 )
 
-# Cargar los datos del archivo: planetas.csv
+# Cargar los datos del archivo: planetas.csv a la tabla planetas de la base de datos scraping
+dataframe = pd.read_csv("Seccion 14 - Web Scraping\planetas.csv")
+print(dataframe)
+
+sql = "INSERT INTO planetas (satelite, planeta, diametro, periodo_orbital) VALUES (%s, %s, %s, %s)"
+cursor = conexion.cursor()
+
+for planeta in dataframe.values:
+    planeta = list(planeta) # Debe ser una lista
+    cursor.execute(sql, planeta)
+    
+conexion.commit()
+cursor.close()
+conexion.close()
